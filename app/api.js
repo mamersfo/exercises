f6c7be545476cefeba000fa8a205ca29ac42e8dd
Martin van Amersfoorth <mamersfo@gmail.com>
2016-12-31 12:20:02 +0100

    Corrected data path

--- app/api.js
+++ app/api.js
@@ -4,7 +4,7 @@ import 'fetch'
 const baseUrl = "https://mamersfo.github.io"
 
 var get = function(which) {
-  let url = `${baseUrl}/blijdorp/data/${which}.json`
+  let url = `${baseUrl}/exercises/data/${which}.json`
   return fetch(url).then(response => response.json())
 }
 
